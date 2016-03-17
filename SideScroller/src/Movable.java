68607a46d3430226812ed10992a9d0fc25230276
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2016-03-17 10:29:47 -0700

    Update Movable.java

--- SideScroller/src/Movable.java
+++ SideScroller/src/Movable.java
@@ -4,11 +4,9 @@ import java.awt.Graphics;
 public class Movable extends GameObjects {
 	///  What do moveable things need to know about themselves???
 	private int x, y, height, width;
-public Movable(int X, int Y, int h, int w){
-	x = X;
-	y = Y;
-	height = h;
-	width = w;
+	private Image image;
+public Movable(int X, int Y, int h, int w, Image i){
+	super(X, Y, h, w, i);
 }
 	public void move() {
 
