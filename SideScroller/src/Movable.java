1a5763dc4c8c3304ae501396cb75e5ceabbc90b0
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2016-03-17 09:48:59 -0700

    Update Movable.java

--- SideScroller/src/Movable.java
+++ SideScroller/src/Movable.java
@@ -19,18 +19,34 @@ public Movable(int X, int Y, int h, int w){
 	}
 	
 	public int get X(){
-		return 
+		return x;
 	}
 	
 	public int get Y(){
-		
+		return y;
 	}
 	
 	public int get height(){
-		
+		return height;
 	}
 	
 	public int get width(){
-		
+		return width;
+	}
+	
+	public void set X(int X){
+		x = X;
+	}
+	
+	public void set Y(int Y){
+		y = Y;
+	}
+	
+	public void set height(int h){
+		height = h;
+	}
+	
+	public void set width(int w){
+		width = w;
 	}
 }
