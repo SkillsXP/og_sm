87e6f0d28db624b184e7d3adbcec097f46603417
wjalex2000 <wjalex2000@gmail.com>
2016-03-21 09:25:21 -0700

    Update MainCharacter.java

--- SideScroller/src/MainCharacter.java
+++ SideScroller/src/MainCharacter.java
@@ -1,4 +1,55 @@
 
-public class MainCharacter extends Movable{
+public class MainCharacter extends Moveable{
+	int x, y, sx, sy;
+	int lives = 1;
+	
+	public MainCharacter(int x, int y, int sx, int sy){
+		//knows its own location and size 
+		//it is rectangle with the image on top of it
+		super(x,y,sx,sy);
+		this.x = x;
+		this.y = y;
+		this.sx = sx;
+		this.sy = sy;
+		}
+	//getters&setters
+	public int getX() {
+		return x;
+	}
 
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSx() {
+		return sx;
+	}
+
+	public void setSx(int sx) {
+		this.sx = sx;
+	}
+
+	public int getSy() {
+		return sy;
+	}
+
+	public void setSy(int sy) {
+		this.sy = sy;
+	}
+
+	public int getLives() {
+		return lives;
+	}
+
+	public void setLives(int lives) {
+		this.lives = lives;
+	}
 }
