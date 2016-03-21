c81058d2b41152d11171cd0c75c06ef421bc3c97
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2016-03-21 11:09:40 -0700

    Update Enemy1.java

--- SideScroller/src/Enemy1.java
+++ SideScroller/src/Enemy1.java
@@ -1,6 +1,20 @@
 public class Enemy1 extends Enemy{
   
-  public Enemy1(){
-    super();
+  private int lives = 1;
+  private boolean hit;
+  public Enemy1(int X, int Y, int h, int w, Image i){
+    super(X, Y, h, w, i);
   }
-}
+  
+  public void isHit(MainCharacter m){
+    if(m.getX() <= this.getX() + this.getWidth() && this.getX() <= m.getX() + m.getWidth() && this.getY() + this.getWidth() + 1 = m.getY()){
+      hit = true;
+    }
+    // check for more stuff such as fireballs here (later)
+  }
+  
+  public void despawn(){
+    if(hit == true){
+      // finish after figuring things out
+    }
+  }
