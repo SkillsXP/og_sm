18ab21beb9766fafabd167c8bed3bd5c5365161c
wjalex2000 <wjalex2000@gmail.com>
2016-03-21 09:36:24 -0700

    Update Platform.java

--- SideScroller/src/Platform.java
+++ SideScroller/src/Platform.java
@@ -1,12 +1,18 @@
 import java.awt.Graphics;
 
-
-public class Platform {
+// this is abstract class for the platform
+//we can make specific platforms like bricks, floating ground and stick (etc...) by extending this thing
+public abstract class Platform {
+	MarioPanel mp;
+	private int lx, ly, sx, sy;
 	
-	
-	
-	public void draw(Graphics g) {
-		
+	public Platform(int locx, int locy, int six, int siy){
+		lx = locx;
+		ly = locy;
+		sx = six;
+		sy = siy;
 	}
+	
+	public abstract void draw(Graphics g); 
 	
 }
