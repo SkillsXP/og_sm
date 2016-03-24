ec5fc44677921eb5d4bd8fe46bb0e0905b0ccc1c
wjalex2000 <wjalex2000@gmail.com>
2016-03-24 09:54:08 -0700

    Create Brick.java

---
+++ SideScroller/src/Brick.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+//this is the brick that breaks
+public abstract class Brick extends Platform{
+	
+	MainCharacter mario;
+	List <Platform> bb = new ArrayList<Platform>();
+	Rectangle brick = null;
+	
+	public Brick(int locx, int locy, int six, int siy, MarioPanel input) {
+		super(locx, locy, six, siy, input);
+		brick = new Rectangle(this.getLocx(), this.getLocy(), this.getWidth(),this.getHeight());
+	}
+
+	@Override
+	public void draw(Graphics g) {
+
+	}
+
+	@Override
+	public void breaks() {
+		//breaks the break if Mario is in certain height and it hits it
+		if ((mario.height == 150) && (brick.intersects(mario.getRmario()))){
+			
+		}
+	}
+	
+
+}
