f68b634ed00312135e3d654672c574c4e0dc38d0
iamscoper <rishdas123@gmail.com>
2016-03-24 09:54:50 -0700

    Create Powerup.java

---
+++ SideScroller/src/Powerup.java
@@ -0,0 +1,13 @@
+public class Powerup{
+  int x;
+  int y;
+  int height;
+  int width;
+  Image image;
+  
+  public Powerup(int X, int Y, int h, int w, Image i)
+  x = X;
+  y = Y;
+  height = h;
+  width = w;
+}
