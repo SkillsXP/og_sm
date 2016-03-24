ab3e5a9a8e6d54f7eb3cc0e75b6320df2442e43c
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2016-03-24 09:51:48 -0700

    Update Enemy.java

--- SideScroller/src/Enemy.java
+++ SideScroller/src/Enemy.java
@@ -1,4 +1,11 @@
+import java.awt.Image;
+
 
 public abstract class Enemy extends Movable{
+
+	public Enemy(int X, int Y, int h, int w, Image i) {
+		super(X, Y, h, w, i);
+		// TODO Auto-generated constructor stub
+	}
 
 }
