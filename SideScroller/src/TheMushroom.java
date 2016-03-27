3aedb0944f0bf00c60f150a7e4914a808a2dd413
wjalex2000 <wjalex2000@gmail.com>
2016-03-27 13:45:03 -0700

    Update TheMushroom.java

--- SideScroller/src/TheMushroom.java
+++ SideScroller/src/TheMushroom.java
@@ -1,15 +1,26 @@
+import java.awt.Rectangle;
+
 
 public class TheMushroom extends Powerups{
 
+	MainCharacter mario;
+	Rectangle marioref;
+	Rectangle msrm = null;
+	
 	public TheMushroom(int width, int height, int locx, int locy) {
 		super(width, height, locx, locy);
-		// TODO Auto-generated constructor stub
+		marioref = new Rectangle(mario.getX(), mario.getY() , mario.getSx(), mario.getSy());
+		msrm = new Rectangle(width,height,locx,locy);
 	}
 
 	@Override
 	public boolean ate() {
-		// TODO Auto-generated method stub
-		return false;
+		if (marioref.intersects(msrm)){
+			mario.setSy(150);
+			return true;
+		}else{
+			return false;
+		}
 	}
-
+	
 }
