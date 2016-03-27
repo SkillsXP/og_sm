801cc36b503b9cf54b11483a32934ef54591971c
wjalex2000 <wjalex2000@gmail.com>
2016-03-27 13:12:22 -0700

    Create TheFlower.java

---
+++ SideScroller/src/TheFlower.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics;
+
+
+public class TheFlower extends Powerups{
+
+	public TheFlower(int width, int height, int locx, int locy) {
+		super(width, height, locx, locy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean ate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void draw(Graphics g){
+		//draw the white mario
+	}
+
+}
