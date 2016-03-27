a3cda282bff42f95c8efded3e838a4aa555c7613
wjalex2000 <wjalex2000@gmail.com>
2016-03-27 13:12:47 -0700

    Create TheMushroom.java

---
+++ SideScroller/src/TheMushroom.java
@@ -0,0 +1,15 @@
+
+public class TheMushroom extends Powerups{
+
+	public TheMushroom(int width, int height, int locx, int locy) {
+		super(width, height, locx, locy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean ate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
