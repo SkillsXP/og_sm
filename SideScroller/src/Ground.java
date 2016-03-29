339445c3a1108578810c3ec65475278c1856dbd4
iamscoper <rishdas123@gmail.com>
2016-03-29 09:21:10 -0700

    Update and rename ground.java to Ground.java

---
+++ SideScroller/src/Ground.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+
+public class Ground extends Platform{
+  MarioPanel mp;
+	private int locx, locy, width, height;
+	private boolean isHit = false;
+	public static List<Platform> p = new List<Platform>();
+	
+	public Ground(int locx, int locy, int six, int siy, MarioPanel input){
+	  super
+}
