f75d0321ee26cfd28f9d539bfd0f7153ab092040
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2016-03-29 09:51:39 -0700

    Update Goomba.java

--- SideScroller/src/Goomba.java
+++ SideScroller/src/Goomba.java
@@ -9,7 +9,7 @@ public class Goomba extends Enemy{
   }
   
   public void isHit(MainCharacter m){
-    if(m.getX() <= this.getX() + this.getWidth() && this.getX() <= m.getX() + m.getWidth() && this.getY() - 1 >= m.getY() + m.getHeight()){
+    if(m.getX() <= this.getX() + this.getWidth() && this.getX() <= m.getX() + m.getWidth() && this.getY() + this.getWidth() + 1 <= m.getY()){
       hit = true;
     }
     // check for more stuff such as fireballs here (later)
@@ -23,7 +23,7 @@ public class Goomba extends Enemy{
   
   public void move(){
 	  int counter = 0;
-		while(collidesWithPlatform() == false){
+		while(collidesWith() == false){
 			if(counter%2 == 0){
 				setX(getX() - 10);
 			}
@@ -32,15 +32,12 @@ public class Goomba extends Enemy{
 			}
 		}
 		counter++;
-  }
+  } 
   
-  public boolean collidesWithPlatform(){
-  	for(int i = 0; i < p.size(); i++){
-  		if(this.getY() + this.getHeight() > p.get(i).getY() && (p.get(i).getX() <= this.getX() + this.getWidth() || this.getX() <= p.get(i).getX() + p.get(i).getWidth())){
-  			return true;	
-  		}
-  		return false;
-  	}
-  }
-  
+  public boolean collidesWithPlatform(Platform p){
+	  		if(this.getY() + this.getHeight() > p.getY() && (p.getX() <= this.getX() + this.getWidth() || this.getX() <= p.getX() + p.getWidth())){
+	  			return true;	
+	  		}
+	  		return false;
+	  }
 }
